f08d878a3a69794e577c4cf863ebad0f907c3f7e
g87736954 <g87736954@gmail.com.tw>
2019-02-05 16:35:01 +0800

    ㄅ

---
+++ shopping/MyCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MyCollectionViewCell.swift
+//  shopping
+//
+//  Created by User on 2019/2/5.
+//  Copyright © 2019 g87. All rights reserved.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var myImageView : UIImageView!
+}
---
+++ shopping/MyTableViewController.swift
@@ -0,0 +1,86 @@
+
+
+//
+//  MyTableViewController.swift
+//  shopping
+//
+//  Created by User on 2019/2/5.
+//  Copyright © 2019 g87. All rights reserved.
+//
+
+import UIKit
+
+class MyTableViewController: UITableViewController,UICollectionViewDelegate ,UICollectionViewDataSource ,UICollectionViewDelegateFlowLayout{
+    
+    let images = ["1","2","3","4","5","6","7","1"]
+    @IBOutlet weak var mycollectionview: UICollectionView!
+    var imageIndex = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // ...In viewDidLoad()
+        // 1.
+        let containerView = UIView()
+        containerView.translatesAutoresizingMaskIntoConstraints = false
+        // headerView is your actual content.
+        containerView.addSubview(headerView)
+        
+        // 2.
+        self.tableView.tableHeaderView = containerView
+        // 3.
+        containerView.centerXAnchor.constraint(equalTo: self.tableView.centerXAnchor).isActive = true
+        containerView.widthAnchor.constraint(equalTo: self.tableView.widthAnchor).isActive = true
+        containerView.topAnchor.constraint(equalTo: self.tableView.topAnchor).isActive = true
+        // 4.
+        self.tableView.tableHeaderView?.layoutIfNeeded()
+        self.tableView.tableHeaderView = self.tableView.tableHeaderView
+        mycollectionview.dataSource = self
+        mycollectionview.delegate = self
+        Timer.scheduledTimer(timeInterval: 2, target: self, selector: #selector(changeBanner), userInfo: nil, repeats: true)
+    }
+    //輪播
+    @objc func changeBanner(){
+        var  indexPath : IndexPath
+        imageIndex += 1
+        if imageIndex < images.count{
+            indexPath  = IndexPath(item: imageIndex, section: 0)
+            mycollectionview.selectItem(at: indexPath, animated: true,scrollPosition: .centeredHorizontally)
+            
+        }else{
+            imageIndex = 0
+            indexPath  = IndexPath(item: imageIndex, section: 0)
+            mycollectionview.selectItem(at: indexPath, animated: false,scrollPosition: .centeredHorizontally)
+            changeBanner()
+        }
+        
+       
+    }
+    
+
+     func numberOfSections(in collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return images.count
+    }
+
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath)as! MyCollectionViewCell
+        cell.myImageView.image=UIImage(named: images[indexPath.item])
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        return collectionView.bounds.size
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return  0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
+        return  0
+    }
+}
